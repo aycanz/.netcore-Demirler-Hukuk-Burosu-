553f9ede67841f855c0bf3248919bad8240e85ef
aycan acar <108690004+aycanz@users.noreply.github.com>
2025-03-03 04:44:05 +0300

    Add files via upload

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,60 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demirler.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IActionResult Index()
+        {
+            var values = bm.GetList();
+            return View(values);
+        }
+
+
+        public IActionResult BlogDetails(int id)
+        {
+            var value = bm.GetBlogById(id); // Tek bir blog döndürmeli
+            return View(value);
+        }
+
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            var values = bm.GetList();
+            return View(values);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BlogAdd(Blog model)
+        {
+            
+             bm.BlogAdd(model);  // Blog verisini ekleme işlemi yapılıyor
+            return RedirectToAction("BlogAdd");
+            ;// return RedirectToAction("Index"); // Başarılıysa anasayfaya yönlendir
+        }
+
+
+    
+
+        public IActionResult Delete(int id)
+        {
+            var blog = bm.GetBlogById(id);
+            bm.BlogDelete(blog);
+            return RedirectToAction("BlogAdd");
+        }
+
+        public IActionResult Deneme()
+        {
+            
+            return View();
+        }
+
+    }
+}
+
