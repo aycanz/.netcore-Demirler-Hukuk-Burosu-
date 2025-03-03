27ded727b920cbfa52214ba27db72dc3eda4694d
aycan acar <108690004+aycanz@users.noreply.github.com>
2025-03-03 04:42:56 +0300

    Add files via upload

---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        
+    [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int YayınID { get; set; }
+
+        public string BaslıkAdı { get; set; }
+        public string Metin { get; set; }
+
+    }
+}
---
+++ EntityLayer/Concrete/Yayınlar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Yayınlar
+
+    {
+        
+    [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int YayınID { get; set; }
+
+        public string BaslıkAdı { get; set; }
+        public string Metin { get; set; }
+
+    }
+}
